6210bae1ddfab3edb300672562d05414c44f169a
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2018-11-16 13:24:21 -0200

    release 0.1.0

--- pages/restaurants/_id.vue
+++ pages/restaurants/_id.vue
@@ -13,13 +13,15 @@
               <img
                 :src="dish.image.url"
                 class="card-img-top"
-                alt="Card image cap"
               >
               <div class="card-body">
                 <h5 class="card-title">{{ dish.name }}</h5>
                 <p class="card-text">{{ dish.description || 'No description provided.' }}</p>
                 <p class="card-text">${{ dish.price }}</p>
-                <button class="btn btn-primary">Add to card</button>
+                <button
+                  class="btn btn-primary"
+                  @click="addToCard(dish)"
+                >Add to card</button>
               </div>
             </div>
           </div>
@@ -28,6 +30,33 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Card</h5>
+              <p class="card-text">{{ numberOfItems }} items selected:</p>
+              <ul>
+                <li
+                  v-for="dish in selectedDishes"
+                  :key="dish.id"
+                  class="card-text mb-2"
+                >
+                  Name: {{ dish.name }} (${{ dish.price }}) ({{ dish.quantity }})
+                  <button
+                    class="btn btn-sm btn-success"
+                    @click="addToCard(dish)"
+                  >+</button>
+                  <button
+                    class="btn btn-sm btn-warning ml-2"
+                    @click="removeFromCard(dish)"
+                  >-</button>
+                </li>
+              </ul>
+              <h5 class="card-text">
+                Total: ${{ price }}
+              </h5>
+              <p v-if="!selectedDishes.length">Please select some items.</p>
+              <button
+                :disabled="!selectedDishes.length"
+                class="btn btn-primary"
+                @click="goToCheckout"
+              >Order</button>
             </div>
           </div>
         </div>
@@ -40,13 +69,28 @@
 import Strapi from 'strapi-sdk-javascript/build/main'
 const apiUrl = process.env.API_URL || 'http://localhost:1337'
 const strapi = new Strapi(apiUrl)
+import { mapMutations } from 'vuex'
 export default {
+  data() {
+    return {
+      complete: false
+    }
+  },
   computed: {
     id() {
       return this.$route.params.id
     },
     dishes() {
       return this.$store.getters['dishes/list']
+    },
+    selectedDishes() {
+      return this.$store.getters['card/items']
+    },
+    price() {
+      return this.$store.getters['card/price']
+    },
+    numberOfItems() {
+      return this.$store.getters['card/numberOfItems']
     }
   },
   async fetch({ store, params }) {
@@ -78,6 +122,22 @@ export default {
         ...dish
       })
     })
+  },
+  methods: {
+    ...mapMutations({
+      addToCard: 'card/add',
+      removeFromCard: 'card/remove',
+      emptyCard: 'card/emptyList'
+    }),
+    goToCheckout() {
+      // Redirect to signin page if not logged in.
+      const isConnected = this.$store.getters['auth/username']
+      if (!isConnected) {
+        this.$router.push('/signin')
+        return
+      }
+      this.$router.push('/checkout')
+    }
   }
 }
 </script>
---
+++ store/index.js
@@ -0,0 +1,16 @@
+import cookieparser from 'cookieparser'
+
+export const actions = {
+  nuxtServerInit({ commit }, { req }) {
+    let user = null
+    let card = []
+    if (req && req.headers && req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      user = (parsed.user && JSON.parse(parsed.user)) || null
+      card = (parsed.card && JSON.parse(parsed.card)) || []
+    }
+
+    commit('auth/setUser', user)
+    commit('card/setItems', card)
+  }
+}
